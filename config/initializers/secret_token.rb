585c8c524dfc89b9ea93df202b4123a2d1025755
Ben fisher <ben@graciouseloise.com>
2013-09-20 14:30:23 +0000

    Initial Commit 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foodtopia::Application.config.secret_key_base = '7771afe86a65b33edef3bd7f03bddd76af2c7a9b304657c5babffe84bd395fbb1ec192d0f81e0d0fa97ac3a218aa62ac960648ad6ae177bb97a2b644667317cd'
