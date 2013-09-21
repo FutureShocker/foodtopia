be75b7abd33cc8068ed9e5d0c3f583edc6e1c2fc
FutureShocker <ben.g.fisher@gmail.com>
2013-09-20 23:57:19 -0400

    Further changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require landing
+//= require jquery-2.0.3.min
+//= require bootstrap.min
